6786b51df7448e0291d77173c13af65913ce842c
pambaka <pambaka@ya.ru>
2024-07-05 09:56:26 +0200

    feat: search button adds the search query to the LS and fetches data

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import './App.css';
 import { Component } from 'react';
 import SearchSection from './search-section/search-section';
 import ResultsSection from './results-section/results-section';
+import { URL } from './consts';
 
 class App extends Component {
+  getPeople = async (searchValue: string = '') => {
+    const searchQuery = searchValue ? `/?search=${searchValue}` : '';
+
+    await fetch(`${URL.people}${searchQuery}`)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
   render() {
     return (
       <>
-        <SearchSection />
+        <SearchSection fetchData={this.getPeople} />
         <ResultsSection />
       </>
     );
--- src/search-section/search-section.tsx
+++ src/search-section/search-section.tsx
@@ -1,14 +1,29 @@
-import { Component } from 'react';
 import './search-section.css';
+import { Component, createRef } from 'react';
 import Button from '../button/button';
+import { SEARCH_VALUE } from '../consts';
 
-class SearchSection extends Component {
+class SearchSection extends Component<{ fetchData: (value: string | undefined) => void }> {
+  inputRef = createRef<HTMLInputElement>();
+
+  handleClick = () => {
+    const value: string | undefined = this.inputRef.current?.value;
+
+    if (value) {
+      localStorage.setItem(SEARCH_VALUE, value);
+    } else {
+      localStorage.removeItem(SEARCH_VALUE);
+    }
+
+    this.props.fetchData(value);
+  };
+
   render() {
     return (
       <>
         <section className="search-section">
-          <input className="search-input" type="text" />
-          <Button buttonText={'Search'} callback={() => console.log('click')} />
+          <input className="search-input" type="text" ref={this.inputRef} />
+          <Button buttonText={'Search'} callback={this.handleClick} />
         </section>
       </>
     );
