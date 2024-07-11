88b2e6c19c8cba9af59cb719d09c03734992749d
pambaka <pambaka@ya.ru>
2024-07-11 23:59:17 +0200

    refactor: add useLocalStorage hook

--- src/search-section/search-section.tsx
+++ src/search-section/search-section.tsx
@@ -1,35 +1,34 @@
 import './search-section.css';
-import { ReactNode, useEffect, useRef } from 'react';
+import { ChangeEvent, ReactNode, useState } from 'react';
 import Button from '../button/button';
-import { SEARCH_VALUE } from '../consts';
+import useLocalStorage from '../hooks/use-local-storage';
 
 function SearchSection(props: {
   fetchData: (value: string | undefined) => Promise<void>;
 }): ReactNode {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useLocalStorage();
+  const [inputValue, setInputValue] = useState(searchValue);
 
   const handleClick = async () => {
-    const value: string | undefined = inputRef.current?.value;
+    setSearchValue(inputValue);
 
-    if (value) {
-      localStorage.setItem(SEARCH_VALUE, value);
-    } else {
-      localStorage.removeItem(SEARCH_VALUE);
-    }
-
-    await props.fetchData(value);
+    await props.fetchData(inputValue);
   };
 
-  useEffect(() => {
-    const searchValue: string | null = localStorage.getItem(SEARCH_VALUE);
-
-    if (searchValue && inputRef.current) inputRef.current.value = searchValue;
-  }, []);
+  const handleChange = (event: ChangeEvent) => {
+    if (event.target instanceof HTMLInputElement)
+      setInputValue(event.target.value);
+  };
 
   return (
     <>
       <section className="search-section">
-        <input className="search-input" type="text" ref={inputRef} />
+        <input
+          className="search-input"
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+        ></input>
         <Button
           buttonText={'Search'}
           callback={() => {
